29d0d51ece83323325e4ec78390d9e4a5e7b1245
tasfique-alam <tasfiquealam@gmail.com>
2021-06-24 23:46:40 +0600

    javascript practice

---
+++ js practice/main.js
@@ -0,0 +1,21 @@
+var type = 'money'
+
+function addNumber(numberOne,numberTwo){
+   return numberOne + numberTwo
+}
+
+
+
+
+
+if(type == true){
+    console.log(addNumber(10,20));
+}else if(type == 'money'){
+    console.log(addNumber(100,200));
+}else{
+    console.log(addNumber(1000,2000));
+}
+
+
+
+
